9ae4bf192d33b2855b7635a8f7f4034b2ae80ed6
Nickolai Nikishin <nickolai.nikishin@trytrue.com>
2021-11-20 11:47:15 +0300

    added ManualLayoutTableViewController

---
+++ Lesson_10/StudentList/StudentList/ManualLayoutTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ManualLayoutTableViewController.swift
+//  StudentList
+//
+//  Created by Nickolai Nikishin on 20.11.21.
+//
+
+import UIKit
+
+class ManualLayoutTableViewController: UIViewController {
+
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        tableView.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

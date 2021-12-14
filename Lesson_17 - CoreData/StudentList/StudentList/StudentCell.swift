cf3d85690e472f02b6561e4733835b6d061de13b
Nickolai Nikishin <nickolai.nikishin@trytrue.com>
2021-12-14 14:09:13 +0300

    lesson 17

---
+++ Lesson_17 - CoreData/StudentList/StudentList/StudentCell.swift
@@ -0,0 +1,37 @@
+//
+//  StudentCell.swift
+//  StudentList
+//
+//  Created by Марина Елисеева on 21.11.21.
+//
+
+import UIKit
+
+class StudentCell: UITableViewCell {
+    let nameLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setup()
+    }
+    
+    private func setup() {
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(nameLabel)
+        
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            nameLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
+        ])
+    }
+}
